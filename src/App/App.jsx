89feb15bde7c82bb97cb750c08cb3af990a897cf
Ridwan Taiwo <donriddo@gmail.com>
2019-03-27 01:06:09 +0100

    Rebuild packages and remove index.css

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -4,6 +4,8 @@ import { Router, Route } from 'react-router-dom';
 import { history } from '../_helpers';
 import { Dashboard } from '../components/Dashboard';
 
+// require('../index.css');
+
 export const App = props => (
     <div className="container-fluid">
         <Router history={history}>
